3583444b4da1398605285cac3ffa87f481361c23
MSI\Bayu <setiawan.irfanbayu@gmail.com>
2021-12-26 21:22:58 +0700

    ubah nama

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className="col-lg-4 mb-5 mb-lg-0">
             <h4 className="text-uppercase mb-4">Location</h4>
             <p className="lead mb-0">
-              Irfan Bayu
+              Irfan Bayu Setiawan
               <br />
               Bekasi, Indonesia
             </p>
